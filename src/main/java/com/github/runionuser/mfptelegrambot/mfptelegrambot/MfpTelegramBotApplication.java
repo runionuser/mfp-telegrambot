dc8b9cf267c5cf1ae7d781c225a8f270d1664a2d
runionuser <abusecsgo@mail.ru>
2024-04-19 12:57:47 +0300

    added files

---
+++ src/main/java/com/github/runionuser/mfptelegrambot/mfptelegrambot/MfpTelegramBotApplication.java
@@ -0,0 +1,13 @@
+package com.github.runionuser.mfptelegrambot.mfptelegrambot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MfpTelegramBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MfpTelegramBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/runionuser/mfptelegrambot/mfptelegrambot/MfpTelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.runionuser.mfptelegrambot.mfptelegrambot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MfpTelegramBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
