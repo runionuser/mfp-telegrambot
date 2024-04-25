e5aecf235a6a23ba8aadc52111a9433786f32e8b
runionuser <abusecsgo@mail.ru>
2024-04-25 12:05:31 +0300

    added stub telegram bot to the project

--- src/main/java/com/github/runionuser/mfptelegrambot/mfptelegrambot/MfpTelegramBotApplication.java
+++ src/main/java/com/github/runionuser/mfptelegrambot/mfptelegrambot/MfpTelegramBotApplication.java
@@ -2,12 +2,14 @@ package com.github.runionuser.mfptelegrambot.mfptelegrambot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class MfpTelegramBotApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MfpTelegramBotApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MfpTelegramBotApplication.class, args);
+    }
 
 }
